a670713440b5479400941284a894a5e4813a0bec
AuSchoolProject <leo.au.90@gmail.com>
2021-05-29 22:33:48 +0200

    :)

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let linkInput = document.querySelector("[data-link-input]");
 function myFunction() {
   navigator.clipboard.writeText(linkInput.placeholder);
 }
+
+function submitAnswer() {
+  let correct = 0;
+  let input = document.querySelectorAll("input[type='radio']checked");
+  for (let i = 0; i < 5; i++) {
+    points += parseInt(input[i].value);
+  }
+  document.querySelector(".play").classList.toggle("invisible");
+  document.querySelector(".play").classList.toggle("invisible");
+}
