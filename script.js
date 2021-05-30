2b66906dc267ba60ab1537b2eb9f6d65a60f4591
AuSchoolProject <leo.au.90@gmail.com>
2021-05-30 23:09:21 +0200

    Final draft

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let linkInput = document.querySelector("[data-link-input]");
+let result = document.querySelector(".result");
+let text = document.createElement("h2");
+result.classList.toggle("invisible");
+result.append(text);
 
 function myFunction() {
   navigator.clipboard.writeText(linkInput.placeholder);
 }
 
 function submitAnswer() {
-  let correct = 0;
-  let input = document.querySelectorAll("input[type='radio']checked");
-  for (let i = 0; i < 5; i++) {
-    points += parseInt(input[i].value);
+  let input = document.querySelector("input[type='radio']:checked");
+  console.log("hej");
+
+  if (input.value == "1") {
+    console.log("hej");
+    text.innerText = "You are right!";
+    document.querySelector(".questions").classList.toggle("invisible");
+    result.classList.toggle("invisible");
+  } else {
+    text.innerText = "You are Wrong!";
+    document.querySelector(".questions").classList.toggle("invisible");
+    result.classList.toggle("invisible");
   }
-  document.querySelector(".play").classList.toggle("invisible");
-  document.querySelector(".play").classList.toggle("invisible");
+}
+
+function reset() {
+  document.querySelector(".questions").classList.toggle("invisible");
+  result.classList.toggle("invisible");
 }
